9cb9862d036ecf95e88f0a75a255330f4f005f16
adriaroca <a.roca.rada@gmail.com>
2019-06-03 14:57:07 +0200

    new delivery addresses table and new columns to customers table

--- database/migrations/2000_create_customers_table.php
+++ database/migrations/2000_create_customers_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateCustomersTable extends Migration
 {
@@ -15,6 +15,62 @@ class CreateCustomersTable extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->date('birthday')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->enum('gender', [
+                'agender',
+                'androgyne',
+                'androgynous',
+                'bigender',
+                'cis',
+                'cisgender',
+                'cis-female',
+                'cis-male',
+                'cis-man',
+                'cis-woman',
+                'cisgender-female',
+                'cisgender-male',
+                'cisgender-man',
+                'cisgender-woman',
+                'female-to-male',
+                'ftm',
+                'gender-fluid',
+                'gender-nonconforming',
+                'gender-questioning',
+                'gender-variant',
+                'genderqueer',
+                'intersex',
+                'male-to-female',
+                'mtf',
+                'neither',
+                'neutrois',
+                'non-binary',
+                'other',
+                'pangender',
+                'trans',
+                'trans-female',
+                'trans-male',
+                'trans-man',
+                'trans-person',
+                'trans-woman',
+                'transfeminine',
+                'transgender',
+                'transgender-female',
+                'transgender-male',
+                'transgender-man',
+                'transgender-person',
+                'transgender-woman',
+                'transmasculine',
+                'transsexual',
+                'transsexual-female',
+                'transsexual-male',
+                'transsexual-man',
+                'transsexual-person',
+                'transsexual-woman',
+                'two-spirit',
+            ])->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
--- tests/BaseTestCase.php
+++ tests/BaseTestCase.php
@@ -1,9 +1,4 @@
 <?php
-/**
- * Company: Omatech
- * User: aroca@omatech.com
- * Creation date: 22/05/19
- */
 
 namespace Omatech\LaravelOrders\Tests;
 
@@ -24,6 +19,7 @@ class BaseTestCase extends \Orchestra\Testbench\TestCase
     {
         include_once __DIR__.'/../database/migrations/1000_create_products_table.php';
         include_once __DIR__.'/../database/migrations/2000_create_customers_table.php';
+        include_once __DIR__.'/../database/migrations/2500_create_customer_delivery_addresses_table.php';
         include_once __DIR__.'/../database/migrations/3000_create_orders_table.php';
         include_once __DIR__.'/../database/migrations/4000_create_order_lines_table.php';
         include_once __DIR__.'/../database/migrations/5000_create_carts_table.php';
@@ -31,6 +27,7 @@ class BaseTestCase extends \Orchestra\Testbench\TestCase
 
         (new \CreateProductsTable())->up();
         (new \CreateCustomersTable())->up();
+        (new \CreateCustomerDeliveryAddressesTable())->up();
         (new \CreateOrdersTable())->up();
         (new \CreateOrderLinesTable())->up();
         (new \CreateCartsTable())->up();
